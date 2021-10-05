c66b47ad97804d391d928647f7e767a03abd4f2a
wlsummers1 <billsumm34@gmail.com>
2021-10-05 11:51:12 -0500

    update text

--- react-test-app-1/src/App.js
+++ react-test-app-1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Updating text
         </a>
       </header>
     </div>
